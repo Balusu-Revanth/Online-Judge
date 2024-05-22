d998e9623f4b74fee62d0eef0ff31df84b07aea7
Balusu-Revanth <balusurevanth5@gmail.com>
2024-05-22 22:59:44 +0530

    Add backend user verification and registration API call

---
+++ online-judge-backend/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { DBConnection } = require("./configs/db");
+const authRoutes = require('./routes/auth');
+const admin = require('firebase-admin');
+const serviceAccountPath = process.env.SERVICE_ACCOUNT_KEY_PATH;
+const serviceAccount = require(serviceAccountPath);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+
+DBConnection();
+
+app.get('/', (req, res) => {
+    res.status(200).send("Server is up and running!!");
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ online-judge-backend/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    uid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
