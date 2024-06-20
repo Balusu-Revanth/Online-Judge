9dfad1513f6fdc757b1d7d22ec6d3adf0422a082
Balusu-Revanth <balusurevanth5@gmail.com>
2024-06-20 19:27:20 +0530

    Update CORS configuration for backend API

--- online-judge-backend/index.js
+++ online-judge-backend/index.js
@@ -17,7 +17,12 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 DBConnection();
 
